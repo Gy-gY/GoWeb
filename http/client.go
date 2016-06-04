fa356a722718c6f2329d758b42a176a9c9fd0334
Gy-gY <yu251923@163.com>
2016-06-04 23:11:52 +0800

    CORE-0604-1: Change 23 -> 123

--- http/client.go
+++ http/client.go
@@ -27,7 +27,7 @@ func main() {
 		log.Fatal("dialing:", err)
 	}
 	
-	args := Args{17, 23}
+	args := Args{17, 123}
 	var reply int
 	err = client.Call("Math.Multiply", args, &reply)
 	if err != nil {
